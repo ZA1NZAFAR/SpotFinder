154c162812c75d9b5d2dd3d71a0ad5c489486d63
Zain ZAFAR <zzs@gmx.com>
2021-05-25 17:38:06 +0200

    added workarount to launch homepage

--- app/src/main/java/dev/zain/spotfinder/MainActivity.kt
+++ app/src/main/java/dev/zain/spotfinder/MainActivity.kt
@@ -1,5 +1,6 @@
 package dev.zain.spotfinder
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
@@ -7,5 +8,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        startActivity(Intent(this, HomePage::class.java))
     }
 }
