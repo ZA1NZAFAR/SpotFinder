db910f991fa4f75cd11a8cfb46e3f45f39fbf0d5
scott <scott.bardolph@gmail.com>
2021-06-09 14:36:42 +0200

    ajouter 3 images, du texte, et 3 boutons images

---
+++ app/src/main/java/dev/zain/spotfinder/DetailsPage.kt
@@ -0,0 +1,11 @@
+package dev.zain.spotfinder
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DetailsPage : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_details_page)
+    }
+}
